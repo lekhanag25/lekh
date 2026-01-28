dccad55f5f4f527014205ed4e16f37cbb8c505e6
lekhanag25 <lekhanag25@gmail.com>
2026-01-28 02:07:01 -0800

    Add files via upload

---
+++ codeone.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class codeone {
+  public static void main(String[] args) {
+	  int x = 0;
+	    float y = 0.0f;
+	    double v = 0.0;
+	    short s = 0;
+	    long l = 0L;
+	    char c = 1
+	    		; 
+	    boolean bool = false;
+	  System.out.println(x);
+	  System.out.println(y);
+	  System.out.println(v);
+	  System.out.println(s);
+	  System.out.println(l);
+	  System.out.println(c);
+	  System.out.println(bool);
+  }
+}
